d1ff213b8d56ce512c6935aed48d05ab99ae95b9
Cody Chen <cdxvy30@gmail.com>
2024-01-14 00:26:51 +0800

    go: 0001

---
+++ leetcode/01_two_sums/sol.go
@@ -0,0 +1,12 @@
+func twoSum(nums []int, target int) []int {
+	ans := []int{0, 0}
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				ans[0] = i
+				ans[1] = j
+			}
+		}
+	}
+	return ans
+}
